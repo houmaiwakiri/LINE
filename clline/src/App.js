2f8323c973766ee641de8bd6fef8d177a5d439ab
houmaiwakiri <mahomahoma888@gmail.com>
2022-02-20 23:45:55 +0900

    時間がないのでちょっと

--- clline/src/App.js
+++ clline/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import SignIn from './components/SignIn';
+import { useAuthState } from "react-firebase-hooks/auth";
+import {auth} from "./firebase.js";
+import Line from './components/Line';
 
 function App() {
+  const user = useAuthState(auth);
   return (
-    <div>
-      <SignIn />
+    <div>   
+      {user ? <Line /> : <SignIn /> } 
     </div>
   );
 }
---
+++ clline/src/components/Line.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Line() {
+  return (
+    <div>Linedesu</div>
+  )
+}
+
+export default Line
