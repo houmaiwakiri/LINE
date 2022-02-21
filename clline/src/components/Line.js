39c5423122030ce4edd3e9556ac49b9d02794b91
houmaiwakiri <mahomahoma888@gmail.com>
2022-02-21 22:24:37 +0900

    データベースからtextをとってくる

--- clline/src/components/Line.js
+++ clline/src/components/Line.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import SignOut from './SignOut';
+import {db} from "../firebase.js";
 
 function Line() {
+  const [messages, SetMessages] = useState([]);
+  useEffect(() => {
+    db.collection("messages")
+    .orderBy("createdAt")
+    .limit(50)
+    .onSnapshot((snapshot) => {
+      SetMessages(snapshot.docs.map((doc) =>doc.data()));
+    })
+  },[]);
   return (
-    <div>Linedesu</div>
+    <div>
+      {console.log(messages)}
+      < SignOut/>
+      <div className='messages'>
+        {messages.map(({id, text ,photoURL, uid}) => (
+          <div>
+            <div key={id}>
+              <img src={photoURL} alt="" />
+              <p>{text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
